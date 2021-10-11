5ac57be21f32d5096e22907b99a285ea13758f0e
Hassan Mehmood <hm.hassanmehmood@gmail.com>
2021-10-11 16:08:03 +0500

    add env variable

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World from {}'.format(os.getenv('VAR_1'))
